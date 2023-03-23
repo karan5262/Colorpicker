148ab64ce980a9cf2b6257fc735fa261c8fe9fe2
karan5262 <75554945+karan5262@users.noreply.github.com>
2023-03-23 18:51:57 +0530

    Add files via upload

---
+++ ask.js
@@ -0,0 +1,5 @@
+function changeColor(){
+    let color = document.getElementById("colorInputColor").Value;
+    document.body.style.backgroundColor=color;
+    document.getElementById("colorInputText").Value=color;
+}
